26481a6304bb1bbfdceee525a706a39c2469a700
Natalya Koshukova <koshukovanat@gmail.com>
2024-01-18 13:12:36 +0700

    ...

--- my-app/src/pages/comparison/Comparison.tsx
+++ my-app/src/pages/comparison/Comparison.tsx
@@ -1,47 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import uuid from 'react-uuid';
 import './СomparisonStyles.css';
-import phones from '../../API/phones.json'
-import Apple_iPhone_12 from '../../images/Apple_iPhone_12.png';
-import Apple_iPhone_Xr from '../../images/Apple_iPhone_Xr.png';
-import Google_Pixel_7A from '../../images/Google_Pixel_7A.png';
-import Realme_8_Pro from '../../images/Realme_8_Pro.png';
-import Samsung_Galaxy_A72 from '../../images/Samsung_Galaxy_A72.png';
-import Samsung_Galaxy_S21 from '../../images/Samsung_Galaxy_S21.png';
-import Xiaomi_Mi_11_Lite_1 from '../../images/Xiaomi_Mi_11_Lite_1.png';
-
 import image_yes from '../../images/yes.png';
 import image_no from '../../images/no.png';
 import image_tick from '../../images/tick.png';
 import image_arrows from '../../images/arrows.png';
-import { log } from 'console';
+import { observer } from 'mobx-react-lite';
+import PhonesStore from '../../store/phonesStore'; 
 
+const Сomparison = observer(() => {
 
-const phonesList = phones.map(phone => ({ ...phone, id: uuid() }));
+	const {phones} = PhonesStore;
 
-const pictures = [Apple_iPhone_12, Xiaomi_Mi_11_Lite_1, Samsung_Galaxy_A72, Samsung_Galaxy_S21, Apple_iPhone_Xr, Realme_8_Pro, Google_Pixel_7A]
+	const renderPictures = phones.map((phone) => {
 
-
-function Сomparison() {
-
-	const [phonesCharacteristics, setPhonesCharacteristics] = useState(phonesList);
-	const [icon, setIcon] = useState(image_yes);
-
-	const renderPictures = phonesCharacteristics.map((phone) => {
-		for (let i = 0; i < pictures.length; i++) {
-				phone.imageName = pictures[i];
-		}
 		return <div className="phones_names" key={phone.id} >
-			<img src={phone.imageName} alt="" className="phone_img_large"></img>
+			<img src={require(`../../images/${phone.imageName}.png`)} alt={phone.imageName} className="phone_img_large"></img>
 			<img src={image_tick} alt="" className="chevron_small"></img>
 			<div className="cell">{phone.name}</div>
 		</div>
 	});
 
-	const renderData = phonesCharacteristics.map((phone) => {
-		// if (phone.nfc === 'no') {
-			// setEmoji(image_no);
-		// }
+	const renderData = phones.map((phone) => {
 		return <div className="phones_data" key={phone.id} >
 			<div className="cell">{phone.manufacturer}</div>
 			<div className="cell">{phone.year}</div>
@@ -50,22 +30,20 @@ function Сomparison() {
 			<div className="cell">{phone.memory}</div>
 			<div className="cell">{phone.frequency}</div>
 			<div className="cell">
-			<img src={icon} alt=""></img>
+				<img src={phone.nfc ? image_yes : image_no} alt=""></img>
 			</div>
 			<div className="cell">
-			<img src={icon} alt=""></img>
+				<img src={phone.eSIM ? image_yes : image_no} alt=""></img>
 			</div>
 			<div className="cell">
-			<img src={icon} alt=""></img>
+				<img src={phone.charging ? image_yes : image_no} alt=""></img>
 			</div>
 			<div className="cell">{phone.price}</div>
 		</div>
 	});
 
-
 	return (
 		<div className="wrapper">
-
 			<div className="phones_main">
 				<div className="phones_smartphones">
 					<div className="phones_title">
@@ -75,20 +53,14 @@ function Сomparison() {
 						Отобразить товары: 2 3 4 5 6
 					</div>
 				</div>
-
 				<div className="phones_models">
-
 					<div className="phones_differences">
 						<input type="checkbox" name="checkbox_differences" />
 						<label htmlFor="checkbox_differences"> Показать различия</label>
 					</div>
-
-					<div className="phones_images">
-						{renderPictures}
-					</div>
+					{renderPictures}
 				</div>
 			</div>
-
 			<div className="phones_characteristics">
 				<div className="phones_characteristics_titles">
 					<div className="characteristic_title">Производитель</div>
@@ -102,10 +74,10 @@ function Сomparison() {
 					<div className="characteristic_title">Поддержка беспроводной зарядки</div>
 					<div className="characteristic_title">Стоимость</div>
 				</div>
-					{renderData}
+				{renderData}
 			</div>
 		</div>
 	);
-}
+})
 
 export default Сomparison;
---
+++ my-app/src/types/stores/phonesStoreTypes.ts
@@ -0,0 +1,15 @@
+export type PhoneType = {
+    id: number;
+    name: string;
+    imageName: string;
+    manufacturer: string;
+    year: string;
+    diagonal: string;
+    country: string;
+    memory: string;
+    frequency: string;
+    nfc: boolean;
+    eSIM: boolean;
+    charging: boolean;
+    price: string
+}
